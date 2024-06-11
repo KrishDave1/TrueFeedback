cdc2bba0ebd2f34d547545449b9977085f885566
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-06-11 16:56:55 -0500

    Next Auth Backend completed

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,11 @@
 /** @format */
 
 import { NextAuthOptions } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { use } from "react";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -15,11 +17,63 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials: any): Promise<any> {
-        const { username, password } = credentials;
-        const user = await UserModel.findOne({
-          username,
-        });
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { username: credentials.identifier },
+              { email: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with the given credentials");
+          }
+
+          if (!user.isVerified) {
+            throw new Error(
+              "User is not verified.Please verify your account with login"
+            );
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isPasswordCorrect) {
+            return user; // Finally CredentialsProvider will return the user object
+          } else {
+            throw new Error("Password is incorrect");
+          }
+        } catch (error: any) {
+          throw new Error("Error in authorizing credentials", error);
+        }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+/** @format */
+
+import { NextRequest, NextResponse } from "next/server";
+// The most simple usage is when you want to require authentication for your entire site.You can add a middleware.js file with the following:
+export { default } from "next-auth/middleware";
+
+import { getToken } from "next-auth/jwt"; // Used to get the JWT token
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  // return NextResponse.redirect(new URL("/home", request.url));
+}
+
+// config is all the locations where this middleware should be applied before running the backend code for the page
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
+};
