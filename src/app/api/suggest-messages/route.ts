19489ec5d8ea4a5129d9ce4ae3a03ebb41ac75e2
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-06-12 21:46:56 -0500

    shadcn setup done

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,42 +1,40 @@
 /** @format */
 
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
-import { NextResponse } from "next/server";
-import OpenAI from "openai";
+//! THIS ROUTE IS NOT WORKING AS OPENAI API REQUIRES A PAID PLAN TO ACCESS THE GPT-3.5-TURBO-INSTRUCT MODEL. THIS PAGE IS MADE ONLY FOR LEARNING PURPOSES AND TO SHOW HOW TO USE OPENAI API IN A CLOUDFLARE WORKER.
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { NextResponse } from "next/server";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   try {
-    // const { messages } = await req.json();
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-    const result = await streamText({
-      model: openai("gpt-3.5-turbo-instruct"),
-      maxTokens: 400,
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      max_tokens: 400,
+      stream: true,
       prompt,
     });
 
-    return result.toAIStreamResponse();
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
   } catch (error) {
     if (error instanceof OpenAI.APIError) {
+      // OpenAI API error handling
       const { name, status, headers, message } = error;
-      return NextResponse.json(
-        {
-          name,
-          status,
-          headers,
-          message,
-        },
-        {
-          status,
-        }
-      );
+      return NextResponse.json({ name, status, headers, message }, { status });
     } else {
-      console.error("An unexpected error occurred");
+      // General error handling
+      console.error("An unexpected error occurred:", error);
       throw error;
     }
   }
