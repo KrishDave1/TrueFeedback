049180f05e04c4376402cee3ee3eddac556d4a62
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-06-12 23:25:14 -0500

    toast element from shadcn added.Progess infrontend

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,43 @@
 /** @format */
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button className= "bg-orange-500 px-3 py-1 m-4 rounded"onClick={() => signIn()}>Sign in</button>
-    </>
-  );
-}
+const Page = () => {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false); //This is to prevent multiple requests to the server
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const debouncedUsername = useDebounceValue(username, 300); //Debounce(delay the change in useState by 300 milliseconds) the username input to prevent multiple requests to the server
+  //This means username will still be updated in real-time but the value of debouncedUsername will only be updated after 300 milliseconds
+  const { toast } = useToast();
+  const router = useRouter();
+
+  //Zod schema for form validation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUsername = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+      }
+    };
+  }, [debouncedUsername]);
+  return <div>page</div>;
+};
+
+export default Page;
